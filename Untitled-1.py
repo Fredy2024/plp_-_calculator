473b4e39b3ff41352875bf67a1828a1d7233d06f
Fredy2024 <fredobonyo5@gmail.com>
2024-03-07 15:37:17 +0300

    percentage calculator

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+def rate_performance(percentage):
+    if percentage >= 90:
+        return "A"
+    elif 80 <= percentage < 90:
+        return "B"
+    elif 70 <= percentage < 80:
+        return "C"
+    elif 60 <= percentage < 70:
+        return "D"
+    else:
+        return "F"
